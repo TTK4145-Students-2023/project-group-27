3ee9a5a140988399feea22f39b220544b45fc46d
Christian Le <crezle1999@gmail.com>
2023-02-01 15:32:50 +0100

    create new branch

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use driver_rust::elevio::elev as e;
+
+fn main() -> std::io::Result<()> {
+    let num_floors = 4;
+    let elevator = e::Elevator::init("localhost:15657", num_floors)?;
+    
+    elevator.motor_direction(e::DIRN_UP);
+    loop {
+        
+    }
 }
