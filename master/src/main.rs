dfb1620b765a5bdb47282408cc729bfe57b00456
William Strom <willistr@stud.ntnu.no>
2023-03-01 16:46:23 +0100

    add network features to master

--- master/src/main.rs
+++ master/src/main.rs
@@ -1,12 +1,32 @@
+use crossbeam_channel::{select};
+use std::time::Duration;
+
 pub mod config;
-pub mod orders;
+// pub mod orders;
+pub mod network;
+
 
 fn main() {
     println!("Master started");
     
-    orders::main();
+    let (receive_hall_order_rx, _receive_elevator_state_rx) = network::init();
+
+
+    println!("Hello");
+    // orders::main();
 
     loop {
-        
+        select! {
+            recv(receive_hall_order_rx) -> msg => {
+                let address = msg.clone().unwrap().address;
+                let destination = msg.clone().unwrap().floor;
+                let button = msg.unwrap().call;
+
+                println!("received order | address {}, destination {}, button {}",address,destination,button);
+            }
+            default(Duration::from_secs(2)) => {
+                println!("Master is still alive!");
+            }
+        }
     }
 }
