2ba6700b19b223f0566633293d21e3a8f581df7f
Christian Le <crezle1999@gmail.com>
2023-03-16 14:20:47 +0100

    add content to new merged module

--- slave/src/prototype_fsm.rs
+++ slave/src/prototype_fsm.rs
@@ -1,5 +1,3 @@
-
-
 use std::time::Duration;
 
 use crossbeam_channel::{Receiver, Sender, select, tick};
@@ -23,14 +21,23 @@ pub struct ElevatorStatus {
     pub direction: u8
 }
 
+#[derive(Clone, Debug)]
+pub struct HallRequests {
+    pub our_requests: Vec<[bool; 2]>,
+    pub all_requests: Vec<[bool; 2]>
+}
+
+
+
 pub fn main(
     elevator_settings: ElevatorSettings,
     cab_button_rx: Receiver<poll::CallButton>, // From io
-    our_hall_requests_rx: Receiver<Vec<[bool; 2]>>, // From network
-    all_hall_requests_rx: Receiver<Vec<[bool; 2]>>, // From network
+    //our_hall_requests_rx: Receiver<Vec<[bool; 2]>>, // From network
+    //all_hall_requests_rx: Receiver<Vec<[bool; 2]>>, // From network
+    hall_requests_rx: Receiver<HallRequests>, //From network
     doors_closing_rx: Receiver<bool>, // From doors
     floor_sensor_rx: Receiver<u8>, // From io
-    cab_requests_tx: Sender<Vec<bool>>, // To io
+    cab_requests_tx: Sender<Vec<bool>>, // To network
     button_light_tx: Sender<(u8,u8,bool)>, // To io
     doors_activate_tx: Sender<bool>, // To doors
     motor_direction_tx: Sender<u8>, // To io
@@ -38,7 +45,7 @@ pub fn main(
     // orders_tx: Sender<Vec<Vec<bool>>>, // To debug
     // cleared_request_tx: Sender<poll::CallButton>, // To network
     // elevator_state_tx: Sender<(String,u8,u8)>, // To network
-    elevator_status_tx: Sender<ElevatorStatus>
+    elevator_status_tx: Sender<ElevatorStatus> // To network & debug
 ) {
 
     let timer = tick(Duration::from_secs_f64(0.1));
@@ -59,16 +66,81 @@ pub fn main(
 
     loop {
         select! {
+            recv(cab_button_rx) -> msg => {
+                State::Idle => {
+                    state = State:DoorOpen;
+                    doors_activate_tx.send(true).unwrap();
+                },
+                State::Moving => {
+                    let destination = msg.clone().unwrap().floor;
+                    // --- Consider turning this part into a function
+                    orders[destination as usize][elev::CAB as usize] = true;
+                    button_light_tx.send((destination, elev::CAB, true)).unwrap();
+                    let mut cab_requests = vec![false; n_floors as usize];
+                    for floor in 0..n_floors {
+                        cab_requests[floor as usize] = orders[floor as usize][elev::CAB as usize];
+                    }
+                    cab_requests_tx.send(cab_requests).unwrap();
+                    // This cab order vector can be unpacked at network instead of here
+                    // --- or a method to a struct
+                },
+                State::DoorOpen => {
+                    doors_activate_tx.send(true).unwrap();
+                },
+                
+            },
+            recv(hall_requests_rx) -> msg => {
+                // --- Consider turning this part into a function
+                State::Idle => {
+                    for f in 0..n_floors {
+                        for b in elev::HALL_UP..=elev::HALL_DOWN {
+                            if msg.clone().unwrap().all_hall_requests[f as usize][b as usize] {
+                                /* Keep door open if elevator gets an order it already is at without sending further. */
+                            }
+                            else {
+                                button_light_tx.send((f, b, msg.clone().unwrap().all_hall_requests[f as usize][b as usize])).unwrap();
+                                orders[f as usize][b as usize] = msg.clone().unwrap().our_hall_requests[f as usize][b as usize];
+                            }
+                            
+                        }
+                    }
+                },
+                State::Moving => {
+
+                },
+                State::DoorOpen => {
+
+                }
+                // --- or a method to the struct
+            },
+            recv() -> _ => {
+
+            },
+            recv() -> _ => {
+
+            },
             recv(timer) -> _ => {
-                orders_tx.send(ElevatorStatus {
-                    /* 
-                    
-                    Fill out content
-                    
-                    */
-                }).unwrap();
+                /* Currently, nothing needs to be sent with timer. */
             }
         }
-        elevator_status_tx.send(/* object */).unwrap();
+
+        // This part is run to get the latest update of states, no matter which recv() was used.
+        let state_str = match state {
+            State::Idle => "idle",
+            State::Moving => "moving",
+            State::DoorOpen => "doorOpen",
+        }; // State is converted to string such that we don't need the state enum dependency at "network"
+        // And also, the string is needed for the hall request assigner. 
+
+        elevator_status_tx.send(ElevatorStatus {
+            orders: orders,
+            cleared_request: poll::CallButton {
+                floor: floor, // This one is from former "elevator_data_rx" in "requests"
+                call: if direction == elev::DIRN_UP { elev::HALL_UP } else { elev::HALL_DOWN },
+            },
+            state: String::from(state_str),
+            floor: floor, // This one is presumed to be the same as the one above
+            direction,
+        }).unwrap();
     }
 }
