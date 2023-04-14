153c9f66716502f3f68302993b1863a9d907dc1b
Finn Ferdinand Sandvand <ffsandva@stud.ntnu.no>
2023-04-14 11:20:42 +0200

    abort on panic and error forwarding in network mod

---
+++ network-rust/src/main.rs
@@ -0,0 +1,117 @@
+use std::env;
+use std::net;
+use std::process;
+use std::thread::*;
+use std::time::Duration;
+
+use crossbeam_channel as cbc;
+use network_rust::udpnet;
+
+// Data types to be sent on the network must derive traits for serialization
+#[derive(serde::Serialize, serde::Deserialize, Debug, Clone)]
+struct CustomDataType {
+    message: String,
+    iteration: u64,
+}
+
+fn main() -> std::io::Result<()> {
+    // Genreate id: either from command line, or a default rust@ip#pid
+    let args: Vec<String> = env::args().collect();
+    let id = if args.len() > 1 {
+        args[1].clone()
+    } else {
+        let local_ip = net::TcpStream::connect("8.8.8.8:53")
+            .unwrap()
+            .local_addr()
+            .unwrap()
+            .ip();
+        format!("rust@{}#{}", local_ip, process::id())
+    };
+
+    let msg_port = 19735;
+    let peer_port = 19738;
+
+    // The sender for peer discovery
+    let (peer_tx_enable_tx, peer_tx_enable_rx) = cbc::unbounded::<bool>();
+    let _handler = {
+        let id = id.clone();
+        spawn(move || {
+            if udpnet::peers::tx(peer_port, id, peer_tx_enable_rx).is_err() {
+                // crash program if creating the socket fails (`peers:tx` will always block if the
+                // initialization succeeds)
+                process::exit(1);
+            }
+        })
+    };
+
+    // (periodically disable/enable the peer broadcast, to provoke new peer / peer loss messages)
+    // This is only for demonstration purposes, if using this module in your project do not include
+    // this
+    spawn(move || loop {
+        sleep(Duration::new(6, 0));
+        peer_tx_enable_tx.send(false).unwrap();
+        sleep(Duration::new(3, 0));
+        peer_tx_enable_tx.send(true).unwrap();
+    });
+
+    // The receiver for peer discovery updates
+    let (peer_update_tx, peer_update_rx) = cbc::unbounded::<udpnet::peers::PeerUpdate>();
+    {
+        spawn(move || {
+            if udpnet::peers::rx(peer_port, peer_update_tx).is_err() {
+                // crash program if creating the socket fails (`peers:rx` will always block if the
+                // initialization succeeds)
+                process::exit(1);
+            }
+        });
+    }
+
+    // Periodically produce a custom data message
+    let (custom_data_send_tx, custom_data_send_rx) = cbc::unbounded::<CustomDataType>();
+    {
+        spawn(move || {
+            let mut cd = CustomDataType {
+                message: format!("Hello from node {}", id),
+                iteration: 0,
+            };
+            loop {
+                custom_data_send_tx.send(cd.clone()).unwrap();
+                cd.iteration += 1;
+                sleep(Duration::new(1, 0));
+            }
+        });
+    }
+    // The sender for our custom data
+    {
+        spawn(move || {
+            if udpnet::bcast::tx(msg_port, custom_data_send_rx).is_err() {
+                // crash program if creating the socket fails (`bcast:tx` will always block if the
+                // initialization succeeds)
+                process::exit(1);
+            }
+        });
+    }
+    // The receiver for our custom data
+    let (custom_data_recv_tx, custom_data_recv_rx) = cbc::unbounded::<CustomDataType>();
+    spawn(move || {
+        if udpnet::bcast::rx(msg_port, custom_data_recv_tx).is_err() {
+            // crash program if creating the socket fails (`bcast:rx` will always block if the
+            // initialization succeeds)
+            process::exit(1);
+        }
+    });
+
+    // main body: receive peer updates and data from the network
+    loop {
+        cbc::select! {
+            recv(peer_update_rx) -> a => {
+                let update = a.unwrap();
+                println!("{:#?}", update);
+            }
+            recv(custom_data_recv_rx) -> a => {
+                let cd = a.unwrap();
+                println!("{:#?}", cd);
+            }
+        }
+    }
+}
---
+++ network-rust/src/udpnet/peers.rs
@@ -0,0 +1,101 @@
+use std::collections::HashMap;
+use std::str;
+use std::time;
+
+use crossbeam_channel as cbc;
+use log::error;
+
+#[path = "./sock.rs"]
+mod sock;
+
+#[derive(Debug)]
+pub struct PeerUpdate {
+    pub peers: Vec<String>,
+    pub new: Option<String>,
+    pub lost: Vec<String>,
+}
+
+/// Periodically broadcast `id` can be toggled with `tx_enable`
+///
+/// Returns `Err` when creating a socket fails. Ignores sending errors after
+/// the socket has been created.
+pub fn tx(port: u16, id: String, tx_enable: cbc::Receiver<bool>) -> std::io::Result<()> {
+    let (s, addr) = sock::new_tx(port)?;
+
+    let mut enabled = true;
+
+    let ticker = cbc::tick(time::Duration::from_millis(15));
+
+    loop {
+        cbc::select! {
+            recv(tx_enable) -> enable => {
+                enabled = enable.unwrap();
+            },
+            recv(ticker) -> _ => {
+                if enabled {
+                    if let Err(e) = s.send_to(id.as_bytes(), &addr) {
+                        error!("Sending failed: {}", e);
+                    }
+                }
+            },
+        }
+    }
+}
+
+/// Forward `id`'s retrieved from the network on port `port`.
+///
+/// Returns `Err` when creating a socket fails. Ignores receiving errors after
+/// creating a socket.
+/// Panics if sending to the channel fails.
+pub fn rx(port: u16, peer_update: cbc::Sender<PeerUpdate>) -> std::io::Result<()> {
+    let timeout = time::Duration::from_millis(500);
+    let s = sock::new_rx(port)?;
+    s.set_read_timeout(Some(timeout))?;
+
+    let mut last_seen: HashMap<String, time::Instant> = HashMap::new();
+    let mut buf = [0; 1024];
+
+    loop {
+        let mut modified = false;
+        let mut p = PeerUpdate {
+            peers: Vec::new(),
+            new: None,
+            lost: Vec::new(),
+        };
+
+        let now = time::Instant::now();
+
+        // Finding new peers
+        if let Ok(n) = s.recv(&mut buf) {
+            if let Ok(id) = str::from_utf8(&buf[..n]) {
+                p.new = if !last_seen.contains_key(id) {
+                    modified = true;
+                    Some(id.to_string())
+                } else {
+                    None
+                };
+                last_seen.insert(id.to_string(), now);
+            }
+        }
+
+        // Finding lost peers
+        for (id, when) in &last_seen {
+            if now - *when > timeout {
+                p.lost.push(id.to_string());
+                modified = true;
+            }
+        }
+        //  .. and removing them
+        for id in &p.lost {
+            last_seen.remove(id);
+        }
+
+        // Sending peer update
+        if modified {
+            p.peers = last_seen.keys().cloned().collect();
+            p.peers.sort();
+            p.lost.sort();
+            peer_update.send(p).unwrap();
+        }
+    }
+}
